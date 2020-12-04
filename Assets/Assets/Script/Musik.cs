05ab61b2b850b42d5693d98345d6fefad5e65b2a
Mahfudin98 <mahfudinnawawi@gmail.com>
2020-12-04 10:04:38 +0700

    first commit

---
+++ Assets/Assets/Script/Musik.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Musik : MonoBehaviour {
+
+	static Musik instance = null;
+	public Slider SliderVolume;
+	public AudioSource MusikBackground;
+
+	private void Awake()	
+	{
+		if (instance != null) 
+		{
+			Destroy(gameObject);
+		} else 
+		{
+			instance = this;
+			GameObject.DontDestroyOnLoad (gameObject);
+		}
+	}
+
+	public void ToggleSound()
+	{
+		if (PlayerPrefs.GetInt ("Muted", 0) == 0) {
+			PlayerPrefs.SetInt ("Muted", 1);
+		} else 
+		{
+			PlayerPrefs.SetInt ("Muted", 0);
+		}
+	}
+
+	public void ChangeVolume(){
+		MusikBackground.volume = SliderVolume.value;
+	}
+}
---
+++ Assets/Assets/Script/URLOpen.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class URLOpen : MonoBehaviour {
+
+	public string Url;
+
+	public void Open(){
+		Application.OpenURL(Url);
+	}
+}
